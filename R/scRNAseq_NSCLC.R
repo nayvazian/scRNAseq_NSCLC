3fcc1ee9114cbf598801fdb462ab41d2ebe309e7
Nicholas Ayvazian <nicholas.ayvazian@gmail.com>
2023-05-07 15:57:20 -0400

    Add files via upload

--- R/scRNAseq_NSCLC.R
+++ R/scRNAseq_NSCLC.R
@@ -1,3 +1,81 @@
+<<<<<<< HEAD
+#load libraries
+library(Seurat)
+library(tidyverse)
+
+#Load the NSCLC dataset
+nsclc.sparse.m <- Read10X_h5(filename = '../gene_expression_data/40k_NSCLC_DTC_3p_HT_nextgem_Multiplex_count_raw_feature_bc_matrix.h5')
+str(nsclc.sparse.m)
+counts <-  nsclc.sparse.m$`Gene Expression`
+
+#Initialize the Seurat object
+nsclc.seurat.obj <- CreateSeuratObject(counts = counts, project = "NSCLC", min.cells = 3, min.features = 200)
+str(nsclc.seurat.obj)
+nsclc.seurat.obj
+
+#QC
+View(nsclc.seurat.obj@meta.data)
+# % MT reads
+nsclc.seurat.obj[["percent.mt"]] <- PercentageFeatureSet(nsclc.seurat.obj, pattern = "^MT-")
+View(nsclc.seurat.obj@meta.data)
+
+VlnPlot(nsclc.seurat.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+FeatureScatter(nsclc.seurat.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") +
+  geom_smooth(method = 'lm')
+
+#Filtering
+nsclc.seurat.obj <- subset(nsclc.seurat.obj, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & 
+                             percent.mt < 5)
+
+
+#Normalize data
+nsclc.seurat.obj <- NormalizeData(nsclc.seurat.obj)
+str(nsclc.seurat.obj)
+
+#Identify highly variable features
+nsclc.seurat.obj <- FindVariableFeatures(nsclc.seurat.obj, selection.method = "vst", nfeatures = 2000)
+#Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(nsclc.seurat.obj), 10)
+#plot variable features with and without labels
+plot1 <- VariableFeaturePlot(nsclc.seurat.obj)
+LabelPoints(plot = plot1, points = top10, repel = TRUE)
+
+#Scaling
+all.genes <- rownames(nsclc.seurat.obj)
+nsclc.seurat.obj <- ScaleData(nsclc.seurat.obj, features = all.genes)
+str(nsclc.seurat.obj)
+
+#Perform Linear dimensionality reduction
+nsclc.seurat.obj <- RunPCA(nsclc.seurat.obj, features = VariableFeatures(object = nsclc.seurat.obj))
+
+#visualize PCA results
+print(nsclc.seurat.obj[["pca"]], dims = 1:5, nfeatures = 5)
+DimHeatmap(nsclc.seurat.obj, dims = 1, cells = 500, balanced = TRUE)
+
+# determine dimensionality of the data
+ElbowPlot(nsclc.seurat.obj)
+
+# 7. Clustering ------------
+nsclc.seurat.obj <- FindNeighbors(nsclc.seurat.obj, dims = 1:15)
+
+# understanding resolution
+nsclc.seurat.obj <- FindClusters(nsclc.seurat.obj, resolution = c(0.1,0.3, 0.5, 0.7, 1))
+View(nsclc.seurat.obj@meta.data)
+
+DimPlot(nsclc.seurat.obj, group.by = "RNA_snn_res.0.5", label = TRUE)
+
+Idents(nsclc.seurat.obj)
+Idents(nsclc.seurat.obj) <- "RNA_snn_res.0.1"
+Idents(nsclc.seurat.obj)
+
+# non-linear dimensionality reduction --------------
+# If you haven't installed UMAP, you can do so via reticulate::py_install(packages =
+# 'umap-learn')
+nsclc.seurat.obj <- RunUMAP(nsclc.seurat.obj, dims = 1:15)
+# note that you can set `label = TRUE` or use the LabelClusters function to help label
+# individual clusters
+DimPlot(nsclc.seurat.obj, reduction = "umap")
+=======
 # load libraries
 library(Seurat)
 library(tidyverse)
@@ -11,3 +89,4 @@ counts <-  nsclc.sparse.m$`Gene Expression`
 nsclc.seurat.obj <- CreateSeuratObject(counts = counts, project = "NSCLC", min.cells = 3, min.features = 200)
 str(nsclc.seurat.obj)
 nsclc.seurat.obj
+>>>>>>> 4484c522580bbe3400eb1af9bb00bc31cd78cb85
